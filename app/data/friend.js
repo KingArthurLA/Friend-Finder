fbe3b890f33b2790e87595db3597f453ce7ac89f
Arthur <Kingarthurla@mail.ru>
2019-03-19 09:38:02 -0700

    initial commit

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Pia Toscano',
+    profilePic: 'https://celebmafia.com/wp-content/uploads/2017/09/pia-toscano-grammy-muesum-gala-in-los-angeles-09-19-2017-9.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
